c8f9b80183f357d52e5d09969e7c647f1ddee7d6
Carlos Henrique <carloshrr1209@gmail.com>
2026-07-23 17:35:55 -0300

    Main view controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,11 @@ module com.main.jfx {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.main.jfx to javafx.fxml;
+    // Permite que o JavaFX acesse a classe Main para iniciar o app
     exports com.main.jfx;
+    opens com.main.jfx to javafx.graphics;
+
+    // Permite que o JavaFX acesse as suas telas e controllers da GUI
+    exports com.main.jfx.gui;
+    opens com.main.jfx.gui to javafx.fxml;
 }
